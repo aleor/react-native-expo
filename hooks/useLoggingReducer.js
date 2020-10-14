36486cdf6bbcd8556e6d785d142b9f911e908cfe
Rustem Akhmetov <aleor2015@gmail.com>
2020-10-14 14:49:09 +0200

    useLoggingReducer

---
+++ hooks/useLoggingReducer.js
@@ -0,0 +1,21 @@
+import { useReducer, useCallback } from 'react';
+
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const useLoggingReducer = (reducer, initialState) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const decoratedDispatch = useCallback(
+    (action) => {
+      console.log(action);
+      dispatch(action);
+    },
+    [dispatch],
+  );
+
+  return [state, decoratedDispatch];
+};
